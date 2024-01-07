0cd273fc297ca3b2906724fb14ff78d72a429dff
filipeartiaga <filipeaartiaga@gmail.com>
2024-01-07 03:06:11 -0300

    fix: fix

--- server/server.js
+++ server/server.js
@@ -2,8 +2,18 @@ const io = require('socket.io')(3000, {
   cors: {
     origin: '*',
   }
-})
+});
 
 io.on('connection', (socket) => {
-  console.log(socket.id);
+  console.log(`New connection: ${socket.id}`);
+
+  // Log disconnect events
+  socket.on('disconnect', () => {
+    console.log(`Disconnected: ${socket.id}`);
+  });
+
+  // Log any errors
+  socket.on('error', (error) => {
+    console.error(`Socket error: ${error}`);
+  });
 });
